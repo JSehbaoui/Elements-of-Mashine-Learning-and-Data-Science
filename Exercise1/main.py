589629b4764962f5d0a0d67a1945697ce291b1ad
Julian Sehbaoui <yassinjulian@gmail.com>
2023-10-31 13:17:23 +0100

    MStep

--- Exercise1/main.py
+++ Exercise1/main.py
@@ -6,6 +6,10 @@ date: 30.10.2023
 import numpy as np
 import math
 
+def getColumn(list, n):
+    if n > np.shape(list)[1]: raise ValueError()
+    return [row[n] for row in list]
+
 def ND(mean, covariance, x):
     K = len(mean)
     det_covariance = np.linalg.det(covariance)
@@ -43,3 +47,30 @@ def EStep(means, covariances, weights, X):
         logLikelihood += np.log(np.sum(gamma[n, :]))
         
     return [logLikelihood, gamma]
+
+def MStep(gamma, X):
+    N, K = np.shape(gamma)
+    D = np.shape(X)[1]  # Dimensionality of the data
+    
+    # Calculate N_hat
+    NHat = np.sum(gamma, axis=0)  # Sum along columns
+    
+    # Calculate weights
+    weights = NHat / N
+    
+    # Calculate means
+    means = np.zeros((K, D))
+    for j in range(K):
+        means[j] = np.sum(gamma[:, j].reshape(-1, 1) * X, axis=0) / NHat[j]
+    
+    # Calculate covariances
+    covariances = np.zeros((K, D, D))
+    for j in range(K):
+        for n in range(N):
+            diff = (X[n] - means[j]).reshape(-1, 1)
+            covariances[j] += gamma[n, j] * np.dot(diff, diff.T)
+        covariances[j] /= NHat[j]
+    
+    logLikelihood = getLogLikelihood(means, weights, covariances, X)
+    
+    return [weights, means, covariances, logLikelihood]
